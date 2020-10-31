54e7a9bdec51a127d3e2bf95d4ab444d61712c31
Vinícius Poloni Crisol <vinicius.crisol@hotmail.com>
2020-10-31 20:35:11 -0300

    first commit

---
+++ src/main/java/com/example/exercios/ExerciosSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercios/ExerciosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
